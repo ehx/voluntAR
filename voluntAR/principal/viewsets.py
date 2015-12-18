6b6f6173a90978bbb70207a390684fdbf221e01e
mcalvino78@gmail.com <mcalvino78@gmail.com>
2015-12-18 18:41:10 -0300

    PostgreSQL

--- voluntAR/principal/viewsets.py
+++ voluntAR/principal/viewsets.py
@@ -1,11 +1,27 @@
-from rest_framework import filters
+from rest_framework import filters, status, permissions
 from rest_framework import generics
 from rest_framework import viewsets
 from .models import Account
 from .serializers import AccountSerializer
+from rest_framework.response import Response
+
+
 
 class AccountViewSet(viewsets.ModelViewSet):
     queryset = Account.objects.all()
     filter_backends = (filters.DjangoFilterBackend,)
     filter_fields = ('id', 'email', 'user_name', 'full_name')
     serializer_class = AccountSerializer
+
+    def create(self, request):
+        serializer = self.serializer_class(data=request.data)
+
+        if serializer.is_valid():
+            Account.objects.create_user(**serializer.validated_data)
+
+            return Response(serializer.validated_data, status=status.HTTP_201_CREATED)
+
+        return Response({
+            'status': 'Bad request',
+            'message': 'Account could not be created with received data.'
+        }, status=status.HTTP_400_BAD_REQUEST)
--- voluntAR/voluntAR/settings.py
+++ voluntAR/voluntAR/settings.py
@@ -76,13 +76,23 @@ WSGI_APPLICATION = 'voluntAR.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.8/ref/settings/#databases
 
+#DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.sqlite3',
+#        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#    }
+#}
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'voluntAR',
+        'USER': 'mauro',
+        'PASSWORD': 'mauro',
+        'HOST': 'localhost',
+        'PORT': '',
     }
-}
 
+}
 
 # Internationalization
 # https://docs.djangoproject.com/en/1.8/topics/i18n/
@@ -106,6 +116,6 @@ STATIC_URL = '/static/'
 AUTH_USER_MODEL = 'principal.Account'
 
 REST_FRAMEWORK = {
-    
+
     'DEFAULT_FILTER_BACKENDS': ['rest_framework.filters.DjangoFilterBackend']
 }
