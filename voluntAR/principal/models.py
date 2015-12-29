f99cc6e9d57a7badd4f439a80266727f8908738f
mcalvino78@gmail.com <mcalvino78@gmail.com>
2015-12-28 22:26:30 -0300

    account

--- voluntAR/principal/models.py
+++ voluntAR/principal/models.py
@@ -12,3 +12,18 @@ class Account(models.Model):
 
     def __str__(self):
         return self.user.first_name  + ' ' + self.user.last_name
+
+class Event(models.Model):
+    owner = models.ForeignKey(Account, null=True, verbose_name='Propietario', related_name="accountEvent")
+    finsh_date = models.DateField(null=True, verbose_name='Fecha de Fin')
+    picture = models.CharField(max_length=500, verbose_name="Imagen")
+    title = models.CharField(max_lenght=50, verbose_name="Titulo")
+    short_description = models.CharField(max_length=150, verbose_name="Descripcion Corta", blank=True)
+    description = models.CharField(max_length=500, verbose_name="Descripcion", blank=True)
+    #location = models.CharField()
+    actions = models.ArrayField(models.CharField(max_length=50), blank=True)
+    elements = model.ArrayField({models.CharField(max_length=30), models.IntegerField}, blank=True) #¿?
+    voluntaries = models.OneToManyField('Account')
+
+    def __str__(self):
+        return self.title
